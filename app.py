3dc24ee48ca073864c021ec93e6a3f7d6b86eeea
kbrien11 <49761400+kbrien11@users.noreply.github.com>
2021-05-13 14:36:45 -0400

    Update app.py

--- app.py
+++ app.py
@@ -38,14 +38,14 @@ ma = Marshmallow(app)
 #     session._model_changes = {}
 #     return session
 
-app.config['MAIL_SERVER'] = 'smtp.sendgrid.net'
-app.config['MAIL_PORT'] = 587
-app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = 'apikey'
-app.config['MAIL_PASSWORD'] = os.getenv("SENDGRID_API_KEY")
-app.config['MAIL_DEFAULT_SENDER'] ='kbrien11@gmail.com'
+# app.config['MAIL_SERVER'] = 'smtp.sendgrid.net'
+# app.config['MAIL_PORT'] = 587
+# app.config['MAIL_USE_TLS'] = True
+# app.config['MAIL_USERNAME'] = 'apikey'
+# app.config['MAIL_PASSWORD'] = os.getenv("SENDGRID_API_KEY")
+# app.config['MAIL_DEFAULT_SENDER'] ='kbrien11@gmail.com'
 
-mail = Mail(app)
+# mail = Mail(app)
 
 
 
