12ca3cbfc1818a4b3e668f67ce6c5cb7374a683a
kbrien11 <49761400+kbrien11@users.noreply.github.com>
2021-05-16 14:27:35 -0400

    Update app.py

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ ENV ='prod'
 if ENV =='prod':
     app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:mbdask1013@localhost/stocks-backend'
 else:
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://amsyitkzdzjkas:e88d822caf94fad99130db7a730fc254c74dc57cebff4d8f7f6d5a385179607e@ec2-54-211-55-24.compute-1.amazonaws.com:5432/d4ejl85c4t0lqd'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://dapeltikrhhuzy:165eac84f310ae14b1649856be3d24e6dca7b24b36b59ee2f9f25102cad32e7d@ec2-34-230-115-172.compute-1.amazonaws.com:5432/d1c51ojbjlm00s'
 
 
 # SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
